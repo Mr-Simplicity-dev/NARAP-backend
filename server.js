7f02a2608313ab2971769b347a0b43057d2dc457
Tahir-DA-Ds <moshood2uall@gmail.com>
2025-07-31 00:36:51 +0100

    cors

--- server.js
+++ server.js
@@ -30,8 +30,28 @@ app.use(compression());
 app.use(morgan('combined'));
 
 // CORS configuration
+const allowedOrigins = [
+  'https://narapdb.com.ng',
+  'http://narapdb.com.ng',
+  'https://www.narapdb.com.ng',
+  'http://www.narapdb.com.ng',
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://localhost:3000',
+  'https://localhost:5173'
+];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'https://narapdb.com.ng',
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
