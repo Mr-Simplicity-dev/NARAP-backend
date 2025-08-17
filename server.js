e5adcbba7a28d78967e7afc5abec9d87459ccbad
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-08-17 07:07:10 -0700

    make changes

--- server.js
+++ server.js
@@ -77,6 +77,8 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100,                 // limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again later.'
+,
+  skip: (req, res) => req.method === 'GET'
 });
 app.use('/api/', limiter);
 
