4c973ecd3aae6cdeda3210c06ee81c20b3b484f6
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-12-23 19:58:13 +0100

    make changes

--- models/SystemLimits.js
+++ models/SystemLimits.js
@@ -4,12 +4,12 @@ const systemLimitsSchema = new mongoose.Schema({
   memberLimit: { 
     type: Number, 
     required: true, 
-    default: 1415  // Change this number whenever you want to increase/decrease
+    default: 1779  // Change this number whenever you want to increase/decrease
   },
   certificateLimit: { 
     type: Number, 
     required: true, 
-    default: 1415   // Change this number whenever you want to increase/decrease
+    default: 1779   // Change this number whenever you want to increase/decrease
   },
   isActive: { 
     type: Boolean, 
